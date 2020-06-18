996a8fffb5c9204c3cfe51d0c5a97790fb65697d
Mateusz <mateusz@kijowski.info>
2020-06-19 01:41:00 +0200

    another dimention

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -415,7 +415,7 @@ void HOT WaveshareEPaperTypeA1::display() {
   int16_t wb = (this->get_width_internal() + 7) / 8;
   for(int i=0; i<this->get_height_internal(); i++) {
     for(int j=0; j<this->get_width_internal()/8; j++) {
-      int idx = j + i * wb;
+      int idx = j + (this->get_height_internal() - 1 - i) * wb;
       this->write_byte(this->buffer_[idx]);
     }
   }
