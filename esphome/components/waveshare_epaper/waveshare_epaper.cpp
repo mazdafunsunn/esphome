ba2cc6399adcbebe27ceb1575bf8a4aaf0c3e7f2
Mateusz <mateusz@kijowski.info>
2020-06-20 22:09:28 +0200

    test

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -412,9 +412,9 @@ void HOT WaveshareEPaperTypeA1::display() {
   // COMMAND WRITE RAM
   this->command(0x24);
   this->start_data_();
-  int16_t wb = (this->get_width_internal() - 7) / 8;
+  int16_t wb = (this->get_width_internal() + 7) / 8;
   for(int i=0; i<this->get_height_internal(); i++) {
-    for(int j=0; j < wb; j++) {
+    for(int j=0; j < this->get_width_internal() / 8; j++) {
       int idx = j + (this->get_height_internal() - 1 - i) * wb;
       this->write_byte(this->buffer_[idx]);
     }
